67096105f66a4c606b27c0225fac389d6e69db76
Ricky Robinett <ricky.robinett@gmail.com>
2012-06-30 17:06:09 +0000

    initial meetup event shiz

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@
   "use strict";
 
   var routes = {
-    root : require("./Root.js"),
-    menu : require("./Menu.js")
+    root  : require("./Root.js"),
+    menu  : require("./Menu.js"),
+    event : require("./Event.js")
   };
 
   var routeList = [
     ["/",                routes.root,           ["get"]],
-    ["/menu/:rid",       routes.menu,           ["get"]]
+    ["/menu/:rid",       routes.menu,           ["get"]],
+    ["/event/:eid",      routes.event,          ["get"]]
   ];
 
   exports.list = routeList;
