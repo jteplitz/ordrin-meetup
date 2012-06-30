7bd07ee5712521b8e4112b3d57c41957373af9e3
Jason Teplitz <jteplitz602@gmail.com>
2012-06-30 11:16:39 -0400

    added bootstrap, jquery, config.json, async, underscore, and ordr.in

---
+++ routes/Menu.js
@@ -0,0 +1,29 @@
+(function(){
+  var _     = require("underscore"),
+     async  = require("async"),
+     Ordrin = require("ordrin-api"),
+     config = require('nconf').argv().env().file({file:'./config.json'});
+
+  var handler, 
+      ordrin = Ordrin.init({
+        apiKey: config.get("api-key"),
+        restaurantUrl: "r-test.ordr.in",
+        userUrl: "u-test.ordr.in",
+        orderUrl: "o-test.ordr.in"
+      })
+
+  handler = function(req, res, next){
+    console.log("called");
+    ordrin.restaurant.getDetails(req.params.rid, function(err, data){
+      if (err){
+        console.log("fuck", err);
+        next(500, err);
+        return;
+      }
+      var params = _.extend({title: data.name}, data);
+      res.render("Menu/index.jade", params);
+    });
+  }
+
+  module.exports = handler;
+})();
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@
 
   var routes = {
     root : require("./Root.js"),
+    menu : require("./Menu.js")
   };
 
   var routeList = [
     ["/",                routes.root,           ["get"]],
+    ["/menu/:rid",       routes.menu,           ["get"]]
   ];
 
   exports.list = routeList;
