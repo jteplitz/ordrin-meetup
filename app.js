158d2e344007950ac0ed9ce832876f623cec636c
Ricky Robinett <ricky.robinett@gmail.com>
2012-06-30 19:11:23 +0000

    fix merge issue

--- app.js
+++ app.js
@@ -59,5 +59,5 @@ _.each(routes.list, function(route){
 
 app.get('/', routes.index);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
